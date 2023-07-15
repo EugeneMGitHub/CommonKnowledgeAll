da63f698e455ae1c6d01941b61fb96210ab4c33e
Eugene <eugenemgithub@gmail.com>
2023-07-15 13:29:23 +0300

    Added some Hilt annotations

---
+++ app/src/main/java/com/example/commonknowledge/AppInstance.kt
@@ -0,0 +1,7 @@
+package com.example.commonknowledge
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AppInstance : Application()
--- app/src/main/java/com/example/commonknowledge/MainActivity.kt
+++ app/src/main/java/com/example/commonknowledge/MainActivity.kt
@@ -11,7 +11,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.commonknowledge.ui.theme.CommonKnowledgeTheme
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
